486812212329b04c9c8f7d6c626b86de84155ffb
yue1123 <2572468699@qq.com>
2023-03-27 13:21:07 +0800

    feat: update test code

--- src/App.vue
+++ src/App.vue
@@ -75,9 +75,19 @@
 			minY: 0,
 			maxY: 2
 		},
-		// {"x":2,"y":0,"w":2,"h":4,"i":"1", resizable: null, draggable: null, static: true},
+		{ x: 2, y: 0, w: 2, h: 4, i: '1', resizable: null, draggable: null, static: true },
 		// {"x":4,"y":0,"w":2,"h":5,"i":"2", resizable: false, draggable: false, static: false, minX: 4, maxX: 4, minW: 2, maxW: 2, preserveAspectRatio: true},
-		// {"x":6,"y":0,"w":2,"h":3,"i":"3", resizable: false, draggable: false, static: false, preserveAspectRatio: true},
+		{
+			x: 6,
+			y: 0,
+			w: 2,
+			h: 3,
+			i: '3',
+			resizable: false,
+			draggable: false,
+			static: false,
+			preserveAspectRatio: true
+		}
 		// { x: 8, y: 0, w: 2, h: 3, i: '4', resizable: false, draggable: false, static: false },
 		// { x: 10, y: 0, w: 2, h: 3, i: '5', resizable: false, draggable: false, static: false }
 		// {"x":0,"y":5,"w":2,"h":5,"i":"6", resizable: false, draggable: false, static: false},
@@ -116,7 +126,7 @@
 				compact: true,
 				restoreOnDrag: true,
 				rowHeight: 30,
-				colNum: 12,
+				colNum: 30,
 				index: 0,
 				marginX: 10,
 				marginY: 10
